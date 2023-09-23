b22dc6ad7023dfd6cd9fb50ba64156ef9c50ffc8
Hingaz <hhinga04@gmail.com>
2023-09-23 22:21:28 +0300

    Completed assignments

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
+function distanceFromHqInBlocks(location) {
+  const hqStreet = 42;
+  return Math.abs(location - hqStreet);
+}
+
 // Code your solution in this file!
+
+function distanceFromHqInFeet(location) {
+  const hqStreet = 42;
+  const feetPerBlock = 264;
+  const distanceInBlocks = Math.abs(location - hqStreet);
+  const distanceInFeet = distanceInBlocks * feetPerBlock;
+  return distanceInFeet;
+}
+
+function distanceTravelledInFeet(startBlock, endBlock) {
+  const feetPerBlock = 264;
+  const distanceInBlocks = Math.abs(endBlock - startBlock);
+  const distanceInFeet = distanceInBlocks * feetPerBlock;
+  return distanceInFeet;
+}
+
+function calculatesFarePrice(start, destination) {
+  const feetPerBlock = 264;
+  const distanceInBlocks = Math.abs(destination - start);
+  const distanceInFeet = distanceInBlocks * feetPerBlock;
+
+  if (distanceInFeet <= 400) {
+      return 0; 
+  } else if (distanceInFeet > 400 && distanceInFeet <= 2000) {
+      
+      return (distanceInFeet - 400) * 0.02;
+  } else if (distanceInFeet > 2000 && distanceInFeet <= 2500) {
+      return 25; 
+  } else {
+      return 'cannot travel that far'; 
+  }
+}
+
+
+
+
+
+
+
+
